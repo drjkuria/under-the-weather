f5db0e46ae04eef284794c412d622dd72a908271
Jonah Kuria <jonah.kuria@hotmail.com>
2017-06-06 16:18:56 +0200

    add cllocation manager delegate method and location auth status method

--- UnderTheWeather/WeatherViewController.swift
+++ UnderTheWeather/WeatherViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import CoreLocation
 
 class WeatherViewController: UIViewController, CLLocationManagerDelegate {
-  
+
   // MARK: - Outlets
   @IBOutlet weak var currentDateLabel: UILabel!
   @IBOutlet weak var currentWeatherImageView: UIImageView!
@@ -18,27 +18,27 @@ class WeatherViewController: UIViewController, CLLocationManagerDelegate {
   @IBOutlet weak var currentWeatherHighLowLabel: UILabel!
   @IBOutlet weak var currentWeatherDescriptionLabel: UILabel!
   @IBOutlet weak var currentLocationLabel: UILabel!
-  
+
   @IBOutlet weak var dayOneForecastNameLabel: UILabel!
   @IBOutlet weak var dayTwoForecastNameLabel: UILabel!
   @IBOutlet weak var dayThreeForecastNameLabel: UILabel!
   @IBOutlet weak var dayFourForecastNameLabel: UILabel!
   @IBOutlet weak var dayFiveForecastNameLabel: UILabel!
-  
+
   @IBOutlet weak var dayOneForecastWeatherImageView: UIImageView!
   @IBOutlet weak var dayTwoForecastWeatherImageView: UIImageView!
   @IBOutlet weak var dayThreeForecastWeatherImageView: UIImageView!
   @IBOutlet weak var dayFourForecastWeatherImageView: UIImageView!
   @IBOutlet weak var dayFiveForecastWeatherImageView: UIImageView!
-  
+
   @IBOutlet weak var dayOneForecastHighLowLabel: UILabel!
   @IBOutlet weak var dayTwoForecastHighLowLabel: UILabel!
   @IBOutlet weak var dayThreeForecastHighLowLabel: UILabel!
   @IBOutlet weak var dayFourForecastHighLowLabel: UILabel!
   @IBOutlet weak var dayFiveForecastHighLowLabel: UILabel!
-  
+
   @IBOutlet weak var separatorView: UIView!
-  
+
   var weatherService: WeatherService!
   let locationManager = CLLocationManager()
   var currentLocation: CLLocation!
@@ -48,35 +48,63 @@ class WeatherViewController: UIViewController, CLLocationManagerDelegate {
     super.viewDidLoad()
     setBlurBackground()
     separatorView.alpha = 0.65
-    
+
     locationManager.delegate = self
     locationManager.desiredAccuracy = kCLLocationAccuracyBest
     locationManager.requestWhenInUseAuthorization()
     locationManager.startUpdatingLocation()
-    
+
     weatherService = WeatherService()
+    locationAuthStatus()
   }
 
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
   }
-  
+
   override var preferredStatusBarStyle: UIStatusBarStyle {
     return UIStatusBarStyle.lightContent
   }
-  
+
   func setBlurBackground() {
     let backgroundImageView = UIImageView(frame: UIScreen.main.bounds)
     backgroundImageView.image = UIImage.init(named: "joburg.jpeg")
     backgroundImageView.contentMode = .scaleAspectFill
     self.view.addSubview(backgroundImageView)
     self.view.sendSubview(toBack: backgroundImageView)
-    
+
     let blurEffect = UIBlurEffect(style: .dark)
     let blurEffectView = UIVisualEffectView(effect: blurEffect)
     blurEffectView.frame = UIScreen.main.bounds
-    
+
     backgroundImageView.addSubview(blurEffectView)
+  }
+
+  func locationAuthStatus() {
+    if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
+      currentLocation = locationManager.location
+      Location.sharedInstance.latitude = currentLocation.coordinate.latitude
+      Location.sharedInstance.longitude = currentLocation.coordinate.longitude
+      weatherService.fetchCurrentWeather { weatherCondition in
+        self.updateCurrentWeather(weatherCondition: weatherCondition)
+        return
+      }
+    } else {
+      locationManager.requestWhenInUseAuthorization()
+      locationAuthStatus()
+    }
+  }
+  
+  func updateCurrentWeather(weatherCondition: WeatherCondition) {
+    print(weatherCondition)
+  }
+  
+  // MARK: - CLLocationManagerDelegate
+  func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+    let location = locations.last as! CLLocation
+    
+    Location.sharedInstance.latitude = location.coordinate.latitude
+    Location.sharedInstance.latitude = location.coordinate.longitude
   }
 
 }
