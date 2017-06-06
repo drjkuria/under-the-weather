6f0fb83e8220097a09bc4723b894368a64f980cb
Jonah Kuria <jonah.kuria@hotmail.com>
2017-06-06 15:41:53 +0200

    add weather service and import alamofire, swiftyjson and corelocation

---
+++ UnderTheWeather/WeatherService.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherService.swift
+//  UnderTheWeather
+//
+//  Created by Jonah Kuria on 2017/06/06.
+//  Copyright © 2017 Jonah Kuria. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+import Alamofire
+import SwiftyJSON
+
+class WeatherService {
+  
+}
