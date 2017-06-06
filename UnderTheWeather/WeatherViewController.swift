d241f9e530bd579e3dba8649b15f4827f2320ec9
Jonah Kuria <jonah.kuria@hotmail.com>
2017-06-06 12:26:11 +0200

    add custom weather view controller file

---
+++ UnderTheWeather/WeatherViewController.swift
@@ -0,0 +1,21 @@
+//
+//  WeatherViewController.swift
+//  UnderTheWeather
+//
+//  Created by Jonah Kuria on 2017/06/06.
+//  Copyright © 2017 Jonah Kuria. All rights reserved.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+  }
+
+}
