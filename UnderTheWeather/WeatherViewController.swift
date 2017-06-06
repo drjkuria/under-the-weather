b4540baaab6b82ad106cc16d58bc6fb4699f489b
Jonah Kuria <jonah.kuria@hotmail.com>
2017-06-06 13:15:44 +0200

    add and connect the outlets

--- UnderTheWeather/WeatherViewController.swift
+++ UnderTheWeather/WeatherViewController.swift
@@ -9,6 +9,34 @@
 import UIKit
 
 class WeatherViewController: UIViewController {
+  
+  // MARK: - Outlets
+  @IBOutlet weak var currentDateLabel: UILabel!
+  @IBOutlet weak var currentWeatherImageView: UIImageView!
+  @IBOutlet weak var currentWeatherTemperatureLabel: UILabel!
+  @IBOutlet weak var currentWeatherHighLowLabel: UILabel!
+  @IBOutlet weak var currentWeatherDescriptionLabel: UILabel!
+  @IBOutlet weak var currentLocationLabel: UILabel!
+  
+  @IBOutlet weak var dayOneForecastNameLabel: UILabel!
+  @IBOutlet weak var dayTwoForecastNameLabel: UILabel!
+  @IBOutlet weak var dayThreeForecastNameLabel: UILabel!
+  @IBOutlet weak var dayFourForecastNameLabel: UILabel!
+  @IBOutlet weak var dayFiveForecastNameLabel: UILabel!
+  
+  @IBOutlet weak var dayOneForecastWeatherImageView: UIImageView!
+  @IBOutlet weak var dayTwoForecastWeatherImageView: UIImageView!
+  @IBOutlet weak var dayThreeForecastWeatherImageView: UIImageView!
+  @IBOutlet weak var dayFourForecastWeatherImageView: UIImageView!
+  @IBOutlet weak var dayFiveForecastWeatherImageView: UIImageView!
+  
+  @IBOutlet weak var dayOneForecastHighLowLabel: UILabel!
+  @IBOutlet weak var dayTwoForecastHighLowLabel: UILabel!
+  @IBOutlet weak var dayThreeForecastHighLowLabel: UILabel!
+  @IBOutlet weak var dayFourForecastHighLowLabel: UILabel!
+  @IBOutlet weak var dayFiveForecastHighLowLabel: UILabel!
+  
+  @IBOutlet weak var separatorView: UIView!
 
   override func viewDidLoad() {
     super.viewDidLoad()
