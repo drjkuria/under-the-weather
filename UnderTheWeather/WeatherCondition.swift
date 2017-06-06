1b476949cb6f38339d6f4a81d53665ba794ab336
Jonah Kuria <jonah.kuria@hotmail.com>
2017-06-06 15:19:56 +0200

    add weather condition struct

---
+++ UnderTheWeather/WeatherCondition.swift
@@ -0,0 +1,20 @@
+//
+//  WeatherCondition.swift
+//  UnderTheWeather
+//
+//  Created by Jonah Kuria on 2017/06/06.
+//  Copyright © 2017 Jonah Kuria. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherCondition {
+  var locationName: String!
+  var description: String!
+  var minTemperature: Int!
+  var maxTemperature: Int!
+  var temperature: Int!
+  var icon: String!
+  var date: String!
+  var countryCode: String!
+}
